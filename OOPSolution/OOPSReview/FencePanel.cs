6352b6adf1980269a529536a1521a4bdac910431
Cy Cerezo :) <54912581+ccerezo2@users.noreply.github.com>
2019-09-06 11:42:39 -0600

    updated day 2

---
+++ OOPSolution/OOPSReview/FencePanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSReview
+{
+    //by default all classes are private
+    //for this class to be used by an outside user(anything but this class)
+    //you must alter the permission for this class 
+    //you must use public class
+    public class FencePanel
+    {
+        
+    }
+}
--- OOPSolution/OOPSReview/Program.cs
+++ OOPSolution/OOPSReview/Program.cs
@@ -10,6 +10,7 @@ namespace OOPSReview
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
