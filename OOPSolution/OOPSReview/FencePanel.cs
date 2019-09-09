75d8278b26db8c088247a358db754525369b01bd
Cy Cerezo :) <54912581+ccerezo2@users.noreply.github.com>
2019-09-09 12:31:02 -0600

    Update FencePanel.cs
    Notes

--- OOPSolution/OOPSReview/FencePanel.cs
+++ OOPSolution/OOPSReview/FencePanel.cs
@@ -12,6 +12,13 @@ namespace OOPSReview
     //you must use public class
     public class FencePanel
     {
-        
+        //properties
+        //A propert is associated with a signle piece of data
+        //a property has 2 subcomponents
+        //get: returns a value to the calling client ( usually from another class )
+        //set: receives a value from the calling client 
+        //     A keyword "Value" is used to hold the incoming data to the property
+        //     the property has a return data type(rdt) which specifies 
+        //     the type of data allowed
     }
 }
